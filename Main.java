087c674caa6adb4789b995f771802e52f4b16334
veermouni <144316777+veermouni@users.noreply.github.com>
2023-09-26 14:16:32 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("welcome devops");
+    System.out.println("welcome to devops");
+    System.out.println("welcome to git hub");
   }
 }
